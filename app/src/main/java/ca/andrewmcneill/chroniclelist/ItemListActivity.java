524b8d5c6fd9ee5499a53e5e4e457e6faf5ff071
ThomasRacz <W0726059@myscc.ca>
2020-03-22 17:05:39 -0400

    commit before merge

--- app/src/main/java/ca/andrewmcneill/chroniclelist/ItemListActivity.java
+++ app/src/main/java/ca/andrewmcneill/chroniclelist/ItemListActivity.java
@@ -47,7 +47,7 @@ import java.util.List;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class ItemListActivity extends AppCompatActivity {
+public class ItemListActivity extends AppCompatActivity  {
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
