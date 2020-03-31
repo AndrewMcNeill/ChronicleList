6e7ca9e1a658d5de039f136f5b12c183b8921b53
ThomasRacz <W0726059@myscc.ca>
2020-03-30 23:14:11 -0400

    #10 Finished DB Helper: * Added Table Schema and Creation * Added All Methods for DB management * Tested DB

--- app/src/main/java/ca/andrewmcneill/chroniclelist/Helpers/DBHelper.java
+++ app/src/main/java/ca/andrewmcneill/chroniclelist/Helpers/DBHelper.java
@@ -1,8 +1,14 @@
 package ca.andrewmcneill.chroniclelist.Helpers;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+import ca.andrewmcneill.chroniclelist.beans.Book;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -13,14 +19,107 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final int DB_VERSION = 1;
     public static final String DB_NAME = "chronicleList";
 
-    // DB Schema
-    public static final String TABLE_NAME = "bookCollections";
+    // Schema DB
+    /*
+     * BOOK NAME
+     * BOOK TITLE
+     * BOOK AUTHOR
+     * BOOK RATING
+     * COVER URL
+     * API ID
+     *
+     */
+    public static final String TABLE_BOOKS = "bookCollections";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_AUTHOR = "author";
+    public static final String COLUMN_RATING = "rating";
+    public static final String COLUMN_IMG_URL = "image";
+    public static final String COLUMN_API_ID = "api_id";
 
-    //TODO: Make Rest of DB Schema
-    
+    public static final String CREATE_BOOK_COLLECTIONS_TABLE = "CREATE TABLE " + TABLE_BOOKS + "(" +  COLUMN_API_ID + " TEXT PRIMARY KEY, "
+            + COLUMN_TITLE + " TEXT, " + COLUMN_AUTHOR + " TEXT, " + COLUMN_RATING + " REAL, " + COLUMN_IMG_URL + " TEXT)";
+
     /*
         Methods
      */
+    public void addBook(Book book) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_TITLE, book.getTitle());
+        values.put(COLUMN_AUTHOR, book.getAuthor());
+        values.put(COLUMN_RATING, book.getRating());
+        values.put(COLUMN_IMG_URL, book.getCoverUrl());
+        values.put(COLUMN_API_ID, book.getApiID());
+
+        db.insert(TABLE_BOOKS, null, values);
+        db.close();
+    }
+
+    public Book getBook(int api_id){
+        SQLiteDatabase db  = this.getReadableDatabase();
+        Book book = null;
+
+        Cursor cursor = db.query(TABLE_BOOKS, new String[]{
+                COLUMN_API_ID,
+                COLUMN_TITLE,
+                COLUMN_AUTHOR,
+                COLUMN_RATING,
+                COLUMN_IMG_URL }, COLUMN_API_ID + "= ?",
+                new String[]{String.valueOf(api_id)}, null, null, null);
+
+        if(cursor.moveToFirst()){
+            // String apiID, String title, String author, double rating, String coverUrl
+
+            book = new Book(
+                    cursor.getString(0),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getInt(3),
+                    cursor.getString(4)
+            );
+        }
+        cursor.close();
+        db.close();
+        return book;
+    }
+
+    public ArrayList<Book> getAllBooks() {
+        ArrayList<Book> allBooks = new ArrayList<>();
+        SQLiteDatabase db  = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_BOOKS,null);
+        while (cursor.moveToNext()) {
+            allBooks.add(
+                new Book(
+                    cursor.getString(0),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getInt(3),
+                    cursor.getString(4)
+            ));
+        }
+        cursor.close();
+        db.close();
+        return allBooks;
+    }
+
+
+    public int updateBookRating(Book book, double rating) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues v = new ContentValues();
+        v.put(COLUMN_RATING, rating);
+        return db.update(TABLE_BOOKS, v, COLUMN_API_ID + "=?",
+                new String[]{String.valueOf( book.getApiID() )});
+    }
+
+
+    public void deleteBook(int api_id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_BOOKS, COLUMN_API_ID + " = ?",
+                new String[]{ String.valueOf(api_id) });
+        db.close();
+    }
+
 
     public DBHelper(Context context){
         super(context, DB_NAME, null, DB_VERSION);
@@ -28,7 +127,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(CREATE_BOOK_COLLECTIONS_TABLE);
     }
 
     @Override
