23d71980a6d75417ff69a914969fa748b20321ce
ThomasRacz <W0726059@myscc.ca>
2020-04-08 15:28:26 -0400

    #30 Added animations for the recycler view: * Added additional method in recycler view adapter to animate each cell in the recycler view individually, fixing the issue of the animation only playing once when attempting to animate via the xml layoutAnimation property

--- app/src/main/java/ca/andrewmcneill/chroniclelist/adapters/BookAdapter.java
+++ app/src/main/java/ca/andrewmcneill/chroniclelist/adapters/BookAdapter.java
@@ -1,5 +1,7 @@
 package ca.andrewmcneill.chroniclelist.adapters;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -92,7 +94,45 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
         Picasso.get().load(books.get(position).getCoverUrl()).into(holder.bookCoverImage);
         holder.itemView.setTag(books.get(position));
         holder.itemView.setOnClickListener(mOnClickListener);
+
+        // add the animation to the itemView (each cell) and play it
+        setAnimator(holder.itemView, position);
     }
+
+    /*  This took too long to figure out:
+     *  So layoutAnimation in the recyclerViews root XML only works when the view is being created, which
+     *  is why animations would only play once. Our recyclerView is static, the only time it's added / removed is when the
+     *  app is launched / closed, otherwise it is never directly altered, only the data changes
+     *
+     *  To get around this problem, we use an ObjectAnimator with an AnimatorSet to animate each individual cell of the recyclerView.
+     *  Because we animate each cell individually, the animation will always play when onBindViewHolder is called.
+     *  https://developer.android.com/reference/android/animation/ObjectAnimator
+     *  https://developer.android.com/reference/android/animation/AnimatorSet
+     *  https://developer.android.com/guide/topics/graphics/prop-animation
+     */
+    private void setAnimator(View itemView, int position) {
+        // set defaults for the itemView initially to avoid that momentary flash of max values when the view is first created
+        itemView.setAlpha(0f);
+        itemView.setTranslationY(300f);
+
+        // make a new animator set to hold all animations
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        // create individual animations as object animators
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(itemView, "alpha", 0f, 1.0f);
+        ObjectAnimator translateY = ObjectAnimator.ofFloat(itemView, "translationY", 300f, 0f);
+
+        // assign a duration and a delay for each item, increase the delay for each item to make the animation smoother (one loads then the next...)
+        translateY.setStartDelay(position * 250 / 4);
+        translateY.setDuration(250);
+
+        // make alpha and translate animations play together
+        animatorSet.playTogether(translateY, alpha);
+
+        // start the animation
+        animatorSet.start();
+    }
+
 
     @Override
     public int getItemCount() {
@@ -133,24 +173,26 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
 
         @Override
         public boolean onLongClick(final View view) {
-            final String bookName = books.get(getLayoutPosition()).getTitle();
-            new AlertDialog.Builder(context)
-                    .setTitle("Remove Book")
-                    .setMessage("Do you want to remove " + bookName + "?")
-                    .setIcon(R.drawable.ic_delete_forever_black_24dp)
-                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            DBHelper db = new DBHelper(context);
-                            db.deleteBook(books.get(getLayoutPosition()).getApiID());
-                            books.remove(getAdapterPosition());
-                            notifyItemRemoved(getAdapterPosition());
-                            db.close();
-                            Snackbar.make(view, bookName + " was removed!", Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();
-                        }
-                    })
-                    .setNegativeButton("No", null).show();
+            if (isStored) {
+                final String bookName = books.get(getLayoutPosition()).getTitle();
+                new AlertDialog.Builder(context)
+                        .setTitle("Remove Book")
+                        .setMessage("Do you want to remove " + bookName + "?")
+                        .setIcon(R.drawable.ic_delete_forever_black_24dp)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                DBHelper db = new DBHelper(context);
+                                db.deleteBook(books.get(getLayoutPosition()).getApiID());
+                                books.remove(getAdapterPosition());
+                                notifyItemRemoved(getAdapterPosition());
+                                db.close();
+                                Snackbar.make(view, bookName + " was removed!", Snackbar.LENGTH_LONG)
+                                        .setAction("Action", null).show();
+                            }
+                        })
+                        .setNegativeButton("No", null).show();
+            }
             return true;
         }
     }
